45740816c939f15ae14b6dc29fcec9f13f5cb10d
BlueZ <62077144+BlueZ-95@users.noreply.github.com>
2020-08-12 17:03:18 +0530

    Replaced contents

---
+++ public/Js/main.js
@@ -0,0 +1,23 @@
+function showFileDetails() {
+    var nameInput = document.getElementById("fileName");
+    //var sizeLabel = document.getElementById("fileSize");
+
+    var selectedFile = document.getElementById('fileInput');
+    nameInput.value = selectedFile.files.item(0).name + ", " + formatBytes(selectedFile.files.item(0).size, 2);
+};
+
+function formatBytes(bytes, decimals = 2) {
+    if (bytes === 0) return '0 Bytes';
+
+    const k = 1024;
+    const dm = decimals < 0 ? 0 : decimals;
+    const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+
+    const i = Math.floor(Math.log(bytes) / Math.log(k));
+
+    return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
+}
+
+$('#uploadModal').on('hide.bs.modal', function () {
+    document.getElementById("fileName").value = "";
+})
